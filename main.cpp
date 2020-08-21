14954c0098bfc8ad565536b5c482190bd2793b3e
sdlas <2749948168@qq.com>
2020-08-21 21:43:23 +0800

    firstcommit

---
+++ main.cpp
@@ -0,0 +1,372 @@
+// Copyright (C) 2018-2019 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/**
+* \brief The entry point for the Inference Engine Human Pose Estimation demo application
+* \file human_pose_estimation_demo/main.cpp
+* \example human_pose_estimation_demo/main.cpp
+*/
+#include <string>
+#include <vector>
+#include <chrono>
+
+#include <inference_engine.hpp>
+
+#include <monitors/presenter.h>
+#include <samples/ocv_common.hpp>
+
+#include "human_pose_estimation_demo.hpp"
+#include "human_pose_estimator.hpp"
+#include "render_human_pose.hpp"
+
+using namespace InferenceEngine;
+using namespace human_pose_estimation;
+//全局变量
+char globaltemp[10];//用于返回关节编号对应的数据
+int arrlength=0;//记录读取的数组数
+double** totalarr =  new double*[10];//总数组
+//函数声明
+char* translate(int k);//将关节编号翻译成数据
+void movetest(){
+    if(totalarr[(arrlength+9)%10][0]&&totalarr[(arrlength+8)%10][0]){
+        std::cout<<"估计距离为"<<totalarr[(arrlength+9)%10][0]-totalarr[(arrlength+8)%10][0]<<std::endl;
+        if(totalarr[(arrlength+9)%10][0]-totalarr[(arrlength+8)%10][0]>10){
+            //std::cout<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<std::endl;
+        }
+    }
+}
+
+void falltest(){
+    if(totalarr[(arrlength+9)%10][3]&&totalarr[(arrlength+8)%10][3]){
+        std::cout<<"估计距离为"<<totalarr[(arrlength+9)%10][0]-totalarr[(arrlength+8)%10][0]<<std::endl;
+        if(totalarr[(arrlength+9)%10][3]-totalarr[(arrlength+8)%10][3]>20){
+            std::cout<<"跌倒了!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<std::endl;
+        }
+    }
+}
+
+bool ParseAndCheckCommandLine(int argc, char* argv[]) {
+    // ---------------------------Parsing and validation of input args--------------------------------------
+
+    gflags::ParseCommandLineNonHelpFlags(&argc, &argv, true);
+    if (FLAGS_h) {
+        showUsage();
+        showAvailableDevices();
+        return false;
+    }
+
+    std::cout << "Parsing input parameters" << std::endl;
+
+    if (FLAGS_i.empty()) {
+        throw std::logic_error("Parameter -i is not set");
+    }
+
+    if (FLAGS_m.empty()) {
+        throw std::logic_error("Parameter -m is not set");
+    }
+
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+
+    double* pointarr = new double[36];
+    for(int k=0;k<10;k++){
+        double* temppoint = new double[36];
+        totalarr[k] = temppoint;
+    }
+    try {
+        std::cout << "InferenceEngine: " << GetInferenceEngineVersion() << std::endl;
+
+        // ------------------------------ Parsing and validation of input args ---------------------------------
+        if (!ParseAndCheckCommandLine(argc, argv)) {
+            return EXIT_SUCCESS;
+        }
+
+        HumanPoseEstimator estimator(FLAGS_m, FLAGS_d, FLAGS_pc);
+        cv::VideoCapture cap;
+        if (!(FLAGS_i == "cam" ? cap.open(0) : cap.open(FLAGS_i))) {
+            throw std::logic_error("Cannot open input file or camera: " + FLAGS_i);
+        }
+
+        int delay = 33;
+
+        // read input (video) frame
+        cv::Mat curr_frame; cap >> curr_frame;
+        cv::Mat next_frame;
+        if (!cap.grab()) {
+            throw std::logic_error("Failed to get frame from cv::VideoCapture");
+        }
+
+        estimator.reshape(curr_frame);  // Do not measure network reshape, if it happened
+
+        std::cout << "To close the application, press 'CTRL+C' here";
+        if (!FLAGS_no_show) {
+            std::cout << " or switch to the output window and press ESC key" << std::endl;
+            std::cout << "To pause execution, switch to the output window and press 'p' key" << std::endl;
+        }
+        std::cout << std::endl;
+
+        cv::Size graphSize{static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH) / 4), 60};
+        Presenter presenter(FLAGS_u, static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT)) - graphSize.height - 10, graphSize);
+        std::vector<HumanPose> poses;
+        bool isLastFrame = false;
+        bool isAsyncMode = false; // execution is always started in SYNC mode
+        bool isModeChanged = false; // set to true when execution mode is changed (SYNC<->ASYNC)
+        bool blackBackground = FLAGS_black;
+
+        typedef std::chrono::duration<double, std::ratio<1, 1000>> ms;
+        auto total_t0 = std::chrono::high_resolution_clock::now();
+        auto wallclock = std::chrono::high_resolution_clock::now();
+        double render_time = 0;
+
+        //while循环中处理视频数据 
+        while (true) {
+            //记录开始工作的时刻点
+            auto t0 = std::chrono::high_resolution_clock::now();
+            //here is the first asynchronus point:
+            //in the async mode（异步模式） we capture frame to populate（填入） the NEXT infer request
+            //一般都不为异步模式，所以 isAsyncMode==0
+            //in the regular mode（一般模式） we capture frame to the current infer request
+            if (!cap.read(next_frame)) {
+                if (next_frame.empty()) {
+                    isLastFrame = true; //end of video file
+                } else {
+                    throw std::logic_error("Failed to get frame from cv::VideoCapture");
+                }
+            }
+
+            //非异步，此段不执行
+            if (isAsyncMode) {
+                std::cout<<"some"<<std::endl;
+                if (isModeChanged) {
+                    estimator.frameToBlobCurr(curr_frame);
+                }
+                if (!isLastFrame) {
+                    estimator.frameToBlobNext(next_frame);
+                }
+            } else if (!isModeChanged) {
+                estimator.frameToBlobCurr(curr_frame);
+            }
+
+            //记录时间点，推算出解码时间
+            auto t1 = std::chrono::high_resolution_clock::now();
+            double decode_time = std::chrono::duration_cast<ms>(t1 - t0).count();
+            
+            //记录解码完成后的时间点
+            t0 = std::chrono::high_resolution_clock::now();
+            // Main sync point:
+            // in the trully Async mode we start the NEXT infer request, while waiting for the CURRENT to complete
+            // in the regular mode we start the CURRENT request and immediately wait for it's completion
+            
+            //非异步，此段不执行
+            if (isAsyncMode) {
+                if (isModeChanged) {
+                    estimator.startCurr();
+                }
+                if (!isLastFrame) {
+                    estimator.startNext();
+                }
+            } else if (!isModeChanged) {
+                estimator.startCurr();
+            }
+            
+            //下面这一大段主要是计算性能，并打印出来
+            if (estimator.readyCurr()) {
+                t1 = std::chrono::high_resolution_clock::now();
+                ms detection = std::chrono::duration_cast<ms>(t1 - t0);
+                t0 = std::chrono::high_resolution_clock::now();
+                ms wall = std::chrono::duration_cast<ms>(t0 - wallclock);
+                wallclock = t0;
+
+                t0 = std::chrono::high_resolution_clock::now();
+
+                //显示性能面板，执行以下打印代码
+                if (!FLAGS_no_show) {
+                    if (blackBackground) {
+                        curr_frame = cv::Mat::zeros(curr_frame.size(), curr_frame.type());
+                    }
+                    std::ostringstream out;
+                    //模型推断所花时间
+                    out << "OpenCV cap/render time: " << std::fixed << std::setprecision(2)
+                        << (decode_time + render_time) << " ms";
+
+                    cv::putText(curr_frame, out.str(), cv::Point2f(0, 25),
+                                cv::FONT_HERSHEY_TRIPLEX, 0.6, cv::Scalar(0, 255, 0));
+                    out.str("");
+                    out << "Wallclock time " << (isAsyncMode ? "(TRUE ASYNC):      " : "(SYNC, press Tab): ");
+                    out << std::fixed << std::setprecision(2) << wall.count()
+                        << " ms (" << 1000.f / wall.count() << " fps)";
+                    cv::putText(curr_frame, out.str(), cv::Point2f(0, 50),
+                                cv::FONT_HERSHEY_TRIPLEX, 0.6, cv::Scalar(0, 0, 255));
+                    if (!isAsyncMode) {  // In the true async mode, there is no way to measure detection time directly
+                        out.str("");
+                        //模型检测所花时间
+                        out << "Detection time  : " << std::fixed << std::setprecision(2) << detection.count()
+                        << " ms ("
+                        << 1000.f / detection.count() << " fps)";
+                        cv::putText(curr_frame, out.str(), cv::Point2f(0, 75), cv::FONT_HERSHEY_TRIPLEX, 0.6,
+                            cv::Scalar(255, 0, 0));
+                    }
+                }
+		        //此处调用骨架推断模型
+                poses = estimator.postprocessCurr();
+
+                //FLAGS_r==false,此段代码不执行
+                if (FLAGS_r) {
+                    if (!poses.empty()) {
+                        std::time_t result = std::time(nullptr);
+                        char timeString[sizeof("2020-01-01 00:00:00: ")];
+                        std::strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S: ", std::localtime(&result));
+                        std::cout << timeString;
+                     }
+
+                    for (HumanPose const& pose : poses) {
+                        std::stringstream rawPose;
+                        rawPose << std::fixed << std::setprecision(0);
+                        for (auto const& keypoint : pose.keypoints) {
+                            rawPose << keypoint.x << "," << keypoint.y << " ";
+                        }
+                        rawPose << pose.score;
+                        std::cout << rawPose.str() << std::endl;
+                    }
+                }
+
+                //此处根据模型推断结果绘制图像，并在视频上显示
+                if (!FLAGS_no_show) {
+                    presenter.drawGraphs(curr_frame);
+                    auto nowtime = std::chrono::high_resolution_clock::now();
+                    ms curtime = std::chrono::duration_cast<ms>(nowtime - total_t0);
+                    //std::cout << "Detection time  : " << std::fixed << std::setprecision(2) << .count()<<std::endl;
+                    renderHumanPose(poses, curr_frame,curtime.count(),pointarr);
+                    cv::imshow("Human Pose Estimation on " + FLAGS_d, curr_frame);
+                    t1 = std::chrono::high_resolution_clock::now();
+                    render_time = std::chrono::duration_cast<ms>(t1 - t0).count();
+                }
+                //将本次数据存入总数组
+                double* temp =  totalarr[arrlength];
+                for(int k=0;k<36;k++){
+                    temp[k] = pointarr[k];
+                }
+                arrlength++;
+                arrlength=arrlength%10;
+                movetest();
+                falltest();
+                // //打印本次数据
+                // for(int k=0;k<18;k++){
+                //     std::cout<<translate(k)<<"的位置是("<<pointarr[2*k]<<","<<pointarr[2*k+1]<<")"<<std::endl;
+                // }
+                // std::cout<<"----line----"<<std::endl;
+            }
+
+            if (isLastFrame) {
+                break;
+            }
+
+            if (isModeChanged) {
+                isModeChanged = false;
+            }
+
+            // Final point:
+            // in the truly Async mode we swap the NEXT and CURRENT requests for the next iteration
+            curr_frame = next_frame;
+            next_frame = cv::Mat();
+            if (isAsyncMode) {
+                estimator.swapRequest();
+            }
+
+	        //键盘控制函数
+            const int key = cv::waitKey(delay) & 255;
+            if (key == 'p') {
+                delay = (delay == 0) ? 33 : 0;
+            } else if (27 == key) { // Esc
+                break;
+            } else if (9 == key) { // Tab
+                isAsyncMode ^= true;
+                isModeChanged = true;
+            } else if (32 == key) { // Space
+                blackBackground ^= true;
+            }
+            presenter.handleKey(key);
+        }
+
+        //计算并打印总视频推断时间（摄像头不执行）
+        auto total_t1 = std::chrono::high_resolution_clock::now();
+        ms total = std::chrono::duration_cast<ms>(total_t1 - total_t0);
+        std::cout << "Total Inference time: " << total.count() << std::endl;
+        std::cout << presenter.reportMeans() << '\n';
+    }
+    //错误处理
+    catch (const std::exception& error) {
+        std::cerr << "[ ERROR ] " << error.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (...) {
+        std::cerr << "[ ERROR ] Unknown/internal exception happened." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "Execution successful" << std::endl;
+    return EXIT_SUCCESS;
+}
+//将数字翻译成对应的部位
+char* translate(int k){
+    switch(k){
+        case 0:
+        strcpy(globaltemp, "鼻子"); 
+        break;
+        case 1:
+        strcpy(globaltemp, "脖子");
+        break;
+        case 2:
+        strcpy(globaltemp, "右肩");
+        break;
+        case 3:
+        strcpy(globaltemp, "右肘");
+        break;
+        case 4:
+        strcpy(globaltemp, "左腕");
+        break;
+        case 5:
+        strcpy(globaltemp, "左肩");
+        break;
+        case 6:
+        strcpy(globaltemp, "左肘");
+        break;
+        case 7:
+        strcpy(globaltemp, "右腕");
+        break;
+        case 8:
+        strcpy(globaltemp, "右臀");
+        break;
+        case 9:
+        strcpy(globaltemp, "右膝");
+        break;
+        case 10:
+        strcpy(globaltemp, "右踝");
+        break;
+        case 11:
+        strcpy(globaltemp, "左臀");
+        break;
+        case 12:
+        strcpy(globaltemp, "左膝");
+        break;
+        case 13:
+        strcpy(globaltemp, "左踝");
+        break;
+        case 14:
+        strcpy(globaltemp, "右眼");
+        break;
+        case 15:
+        strcpy(globaltemp, "左眼");
+        break;
+        case 16:
+        strcpy(globaltemp, "右耳");
+        break;
+        case 17:
+        strcpy(globaltemp, "左耳");
+        break;
+    }
+    return globaltemp;
+}
---
+++ src/render_human_pose.cpp
@@ -0,0 +1,157 @@
+// Copyright (C) 2018-2019 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#include<cmath>
+#include<string>
+#include <utility>
+#include <vector>
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <fstream>
+#include <iostream>
+#include "human_pose_estimator.hpp"
+#include "render_human_pose.hpp"
+//全局变量
+double temppointarr[18][2];
+double curpointarr[18][2];
+//函数声明
+double distance(double x1,double y1,double x2,double y2);//计算两点间的距离
+void zerotemparr();//将临时数组重置
+void zerocurarr();//将当前数组重置
+double calheight();//计算人的大致高度
+bool exist(double k);//判断某个关键点是否存在
+//将数组重置
+//计算两点间的距离
+double distance(double x1,double y1,double x2,double y2){
+    return sqrt(pow(x1-x2,2)+pow(y1-y2,2));
+}
+void zerotemparr(){
+    for(int k =0;k<18;k++){
+        temppointarr[k][0]=-1.0;
+        temppointarr[k][1]=-1.0;
+    }
+}
+//将当前数组重置
+void zerocurarr(){
+    for(int k =0;k<18;k++){
+        curpointarr[k][0]=-1.0;
+        curpointarr[k][1]=-1.0;
+    }
+}
+//计算人的大致高度
+double calheight(){
+    //如果脖子和臀部在的话，直接计算
+    if(exist(temppointarr[1][1])&&exist(temppointarr[11][1])){
+        return distance(temppointarr[1][0],temppointarr[1][1],temppointarr[11][0],temppointarr[11][1])*3;
+    }else if(exist(temppointarr[1][1])&&exist(temppointarr[8][1])){
+        return distance(temppointarr[1][0],temppointarr[1][1],temppointarr[8][0],temppointarr[8][1])*3;
+    }
+    //如果脖子和膝盖都在
+    if(exist(temppointarr[1][1])&&exist(temppointarr[12][1])){
+        return distance(temppointarr[1][0],temppointarr[1][1],temppointarr[12][0],temppointarr[12][1])*2;
+    }else if(exist(temppointarr[1][1])&&exist(temppointarr[9][1])){
+        return distance(temppointarr[1][0],temppointarr[1][1],temppointarr[9][0],temppointarr[9][1])*2;
+    }
+    return 0.0;
+}
+//判断某个点是否存在
+bool exist(double k){
+    return k!=-1.0;
+}
+namespace human_pose_estimation {
+void renderHumanPose(const std::vector<HumanPose>& poses, cv::Mat& image,double curtime,double* pointarr) {
+    CV_Assert(image.type() == CV_8UC3);
+    std::cout << curtime<<std::endl;
+    static const cv::Scalar colors[HumanPoseEstimator::keypointsNumber] = {
+        cv::Scalar(255, 0, 0), cv::Scalar(255, 85, 0), cv::Scalar(255, 170, 0),
+        cv::Scalar(255, 255, 0), cv::Scalar(170, 255, 0), cv::Scalar(85, 255, 0),
+        cv::Scalar(0, 255, 0), cv::Scalar(0, 255, 85), cv::Scalar(0, 255, 170),
+        cv::Scalar(0, 255, 255), cv::Scalar(0, 170, 255), cv::Scalar(0, 85, 255),
+        cv::Scalar(0, 0, 255), cv::Scalar(85, 0, 255), cv::Scalar(170, 0, 255),
+        cv::Scalar(255, 0, 255), cv::Scalar(255, 0, 170), cv::Scalar(255, 0, 85)
+    };
+    static const std::pair<int, int> limbKeypointsIds[] = {
+        {1, 2},  {1, 5},   {2, 3},
+        {3, 4},  {5, 6},   {6, 7},
+        {1, 8},  {8, 9},   {9, 10},
+        {1, 11}, {11, 12}, {12, 13},
+        {1, 0},  {0, 14},  {14, 16},
+        {0, 15}, {15, 17}
+    };
+
+    const int stickWidth = 4;
+    const cv::Point2f absentKeypoint(-1.0f, -1.0f);
+    //写入文件版
+    // std::ofstream OpenFile("out.txt",std::ios::out|std::ios::app);
+    // for (const auto& pose : poses) {
+    //     CV_Assert(pose.keypoints.size() == HumanPoseEstimator::keypointsNumber);
+    //     OpenFile<<"第"<<i+1<<"个人"<<'\n';
+    //     for (size_t keypointIdx = 0; keypointIdx < pose.keypoints.size(); keypointIdx++) {
+    //         if (pose.keypoints[keypointIdx] != absentKeypoint) {
+    //             cv::circle(image, pose.keypoints[keypointIdx], 4, colors[keypointIdx], -1);
+    //             OpenFile<<"typeid="<<pose.keypoints[keypointIdx].x<<","<<pose.keypoints[keypointIdx].y<<'\n';
+    //             pointlist[keypointIdx][0] = pose.keypoints[keypointIdx].x;
+    //             pointlist[keypointIdx][1] = pose.keypoints[keypointIdx].y;
+    //         }
+    //     }
+    //     i++;
+    // }
+    // OpenFile<<"-----opline-----"<<'\n';	
+    // OpenFile.close();
+    //直接打印版
+    //将两个数组初始化
+    zerotemparr();
+    double maxheight=0;//当前检测到的最大身高,如果不超出太多的话则会记入
+    for (const auto& pose : poses) {
+        CV_Assert(pose.keypoints.size() == HumanPoseEstimator::keypointsNumber);
+        for (size_t keypointIdx = 0; keypointIdx < pose.keypoints.size(); keypointIdx++) {
+            if (pose.keypoints[keypointIdx] != absentKeypoint) {
+                cv::circle(image, pose.keypoints[keypointIdx], 4, colors[keypointIdx], -1);
+                //将当前读取的点的数据放入临时数组
+                temppointarr[keypointIdx][0] = pose.keypoints[keypointIdx].x;
+                temppointarr[keypointIdx][1] = pose.keypoints[keypointIdx].y;
+                // pointarr[keypointIdx*2] = pose.keypoints[keypointIdx].x;
+                // pointarr[keypointIdx*2+1] = pose.keypoints[keypointIdx].y;
+            }
+        }
+        //现在只看一个人的状态,需要过滤掉其他人的干扰
+        //估算人的大致身高
+        double tempheight = calheight();
+        if(maxheight!=0.0){
+            //因为人在移动后身高会随着离摄像头的距离改变而改变
+            if(tempheight>0.9*maxheight&&tempheight<1.1*maxheight){
+                maxheight = tempheight;
+            }
+        }else{
+            maxheight = tempheight;
+            for(int k=0;k<18;k++){
+                pointarr[k*2] = temppointarr[k][0];
+                pointarr[k*2+1] = temppointarr[k][1];
+            }
+        }
+    }
+    std::cout<<"估计身高为:"<<maxheight<<std::endl;
+    cv::Mat pane = image.clone();
+    for (const auto& pose : poses) {
+        for (const auto& limbKeypointsId : limbKeypointsIds) {
+            std::pair<cv::Point2f, cv::Point2f> limbKeypoints(pose.keypoints[limbKeypointsId.first],
+                    pose.keypoints[limbKeypointsId.second]);
+            if (limbKeypoints.first == absentKeypoint
+                    || limbKeypoints.second == absentKeypoint) {
+                continue;
+            }
+
+            float meanX = (limbKeypoints.first.x + limbKeypoints.second.x) / 2;
+            float meanY = (limbKeypoints.first.y + limbKeypoints.second.y) / 2;
+            cv::Point difference = limbKeypoints.first - limbKeypoints.second;
+            double length = std::sqrt(difference.x * difference.x + difference.y * difference.y);
+            int angle = static_cast<int>(std::atan2(difference.y, difference.x) * 180 / CV_PI);
+            std::vector<cv::Point> polygon;
+            cv::ellipse2Poly(cv::Point2d(meanX, meanY), cv::Size2d(length / 2, stickWidth),
+                             angle, 0, 360, 1, polygon);
+            cv::fillConvexPoly(pane, polygon, colors[limbKeypointsId.second]);
+        }
+    }
+    cv::addWeighted(image, 0.4, pane, 0.6, 0, image);
+}
+}  // namespace human_pose_estimation
